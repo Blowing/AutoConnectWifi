43fbb91213bbda90e178e934970629ea82434f48
Administrator <1072878103@qq.com>
2016-08-28 23:19:18 +0800

    上传工具类

---
+++ app/src/main/java/com/wujie/autoconnectwifi/WifiUtil.java
@@ -0,0 +1,182 @@
+package com.wujie.autoconnectwifi;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.widget.Switch;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/8/28.
+ */
+public class WifiUtil {
+
+    private WifiManager mWifiManager;
+
+    private WifiInfo mWifiInfo;
+
+    private List<ScanResult> mWifiList;
+
+    private List<WifiConfiguration> mWificonfiguration;
+
+    private WifiManager.WifiLock mWifiLock;
+
+
+    public WifiUtil(Context context) {
+        mWifiManager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+
+        mWifiInfo = mWifiManager.getConnectionInfo();
+    }
+
+    public void openWifi() {
+        if (!mWifiManager.isWifiEnabled()) {
+            mWifiManager.setWifiEnabled(true);
+        }
+    }
+
+    public int checkState() {
+        return mWifiManager.getWifiState();
+    }
+
+    public void acquireWifiLoc() {
+        mWifiLock.acquire();
+    }
+
+    public void releaseWifiLock() {
+        if(mWifiLock.isHeld()) {
+            mWifiLock.acquire();
+        }
+    }
+
+    public void createWifiLock() {
+        mWifiLock = mWifiManager.createWifiLock("test");
+    }
+
+    public List<WifiConfiguration> getConfigurations () {
+        return  mWificonfiguration;
+    }
+
+    public void connectConfiguration(int index) {
+
+        if(index > mWificonfiguration.size()) {
+            return;
+        }
+
+        mWifiManager.enableNetwork(mWificonfiguration.get(index).networkId, true);
+    }
+
+    public void startScan() {
+        mWifiManager.startScan();
+        mWifiList = mWifiManager.getScanResults();
+        mWificonfiguration = mWifiManager.getConfiguredNetworks();
+    }
+
+    public List<ScanResult> getmWifiList() {
+        return mWifiList;
+    }
+
+    public StringBuilder lookUpScan() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < mWifiList.size(); i++) {
+            stringBuilder.append("Index_" + String.valueOf(i+1)+":");
+            stringBuilder.append(mWifiList.get(i).toString());
+            stringBuilder.append("/n");
+        }
+        return  stringBuilder;
+    }
+
+    public String getMacAddress() {
+        return  (mWifiInfo == null) ? "NULL" : mWifiInfo.getMacAddress();
+    }
+
+    public String getSSID() {
+        return  (mWifiInfo == null) ? "NULL" : mWifiInfo.getSSID();
+    }
+
+    public int getIpAddress() {
+        return (mWifiInfo == null) ? 0 : mWifiInfo.getIpAddress();
+    }
+
+    public int getNetworkId() {
+        return (mWifiInfo == null) ? 0 : mWifiInfo.getNetworkId();
+
+    }
+
+    public  String getWifiInfo() {
+        return (mWifiInfo == null) ? "NULL" : mWifiInfo.toString();
+    }
+
+    public void addNetWork(WifiConfiguration wifiConfiguration) {
+        int wcgID = mWifiManager.addNetwork(wifiConfiguration);
+        boolean b = mWifiManager.enableNetwork(wcgID, true);
+
+    }
+
+    public void disconnectWifi(int netId) {
+        mWifiManager.disableNetwork(netId);
+        mWifiManager.disconnect();
+    }
+
+    public WifiConfiguration createWifiInfo(String SSID, String Password, int Type) {
+        WifiConfiguration configuration = new WifiConfiguration();
+        configuration.allowedAuthAlgorithms.clear();
+        configuration.allowedGroupCiphers.clear();
+        configuration.allowedKeyManagement.clear();
+        configuration.allowedPairwiseCiphers.clear();
+        configuration.allowedProtocols.clear();
+        configuration.SSID = "\"" + SSID + "\"";
+
+        WifiConfiguration tempConfig = this.isExsits(SSID);
+        if(tempConfig != null) {
+            mWifiManager.removeNetwork(tempConfig.networkId);
+        }
+
+        switch (Type) {
+            case 1:
+                configuration.wepKeys[0] = "";
+                configuration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+                configuration.wepTxKeyIndex = 0;
+                break;
+            case 2:
+                configuration.hiddenSSID = true;
+                configuration.wepKeys[0] = "\"" + Password +"\"";
+                configuration.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
+                configuration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+                configuration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+                configuration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+                configuration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+                configuration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+
+                break;
+            case 3:
+                configuration.preSharedKey = "\"" + Password + "\"";
+                configuration.hiddenSSID = true;
+                configuration.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+                configuration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+                configuration.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+                configuration.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+                configuration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+                configuration.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+                configuration.status = WifiConfiguration.Status.ENABLED;
+                break;
+        }
+        return  configuration;
+    }
+
+    private WifiConfiguration isExsits(String SSID) {
+        List<WifiConfiguration> existingConfigs = mWifiManager.getConfiguredNetworks();
+        for (WifiConfiguration existingConfig:
+             existingConfigs) {
+            if (existingConfig.SSID.equals("\"" +SSID+"\"")) {
+                return  existingConfig;
+            }
+            
+        }
+        return null;
+    }
+
+
+}
